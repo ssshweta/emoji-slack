72656e023eadc7e5e8a99a168dcff8b2383e6a74
Daniel Lopez <Daniel_Lopez@intuit.com>
2019-04-17 16:17:31 -0700

    refactored for multiple output types; also did single word/phrase-emoji replacement

--- espell.py
+++ espell.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python3
-
+import sys
 import json
 import random
 
+def getEmojifiedVersion(message):
+    with open('/Users/dlopez7/dev/emoji-slack/translator.json') as t_file:
+        translator = json.load(t_file)
+        newMessage = ''
 
-with open('translator.json') as t_file:
-    translator = json.load(t_file)
+        for letter in message:
+            if letter.lower() in translator:
+                options = translator[letter.lower()]
+                newMessage += random.choice(options)
+            else:
+                newMessage += letter
+        
+        return newMessage
 
-    originalMessage = input()
-    newMessage = ''
-    
-    for letter in originalMessage:
-        if letter.lower() in translator:
-            options = translator[letter.lower()]
-            newMessage += random.choice(options)
-        else:
-            newMessage += letter
-    
-    print(newMessage)
+def pepperWithEmojis(message):
+    with open('/Users/dlopez7/dev/emoji-slack/emoji-list.json') as t_file:
+        replacements = json.load(t_file)
+        newMessage = message.lower()
+
+        for replacement in replacements:
+            newMessage = newMessage.replace(replacement, ":{}:".format(replacement))
+        
+        return newMessage
+
+if __name__ == "__main__":
+    originalMessage = input("Enter message: ")
+    print(getEmojifiedVersion(originalMessage))
+    print(pepperWithEmojis(originalMessage))
+
